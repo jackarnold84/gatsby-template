11aea29a047c62f00b249e328ffa88617fc58fb3
Jack Arnold <jackarnold84@gmail.com>
2023-04-27 17:47:46 -0500

    Updates

---
+++ component---src-pages-fruits-[name]-js-70be3e99e61b6d608f3e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[428],{2596:function(e,t,n){n.r(t),n.d(t,{Head:function(){return l}});var a=n(7294);t.default=e=>{const t=e.params.name;return a.createElement("main",null,a.createElement("h1",null,t," page"),a.createElement("p",null,"I hope you enjoy this fruit"))};const l=()=>a.createElement("title",null,"Fruit Page")}}]);
+//# sourceMappingURL=component---src-pages-fruits-[name]-js-70be3e99e61b6d608f3e.js.map
