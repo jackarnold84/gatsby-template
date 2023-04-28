8600b6afb24c032547f186bf1410f11c8985efb7
Jack Arnold <jackarnold84@gmail.com>
2023-04-28 11:43:29 -0500

    add css

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
+import '../styles/global.css'
+import '../styles/icons.css'
+
+const veggieFontStyle = {
+  color: '#37b635',
+  fontSize: '24px',
+}
 
 const AboutPage = () => {
   const numbers = [1, 2, 3, 4]
   return (
     <main>
       <h1>About Page</h1>
-      <p>This is my about page</p>
+      <p className="veggie">This is my about page</p>
+      <p>
+        <i className="bi-cloud" />
+      </p>
+      <p style={veggieFontStyle}>Celery</p>
       <Link to="/">
         <h3>Return Home</h3>
       </Link>
