a4dfc7ed2ed6bc7f24054edbbb239a6a6f695431
jackarnold84 <jackarnold84@gmail.com>
2025-03-15 16:56:19 -0500

    Updates

--- component---src-pages-about-js-ddfb55bdb916108f4e9c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[682],{5465:function(e,t,n){n.r(t),n.d(t,{Head:function(){return u}});var l=n(7294),a=n(1883);t.default=()=>l.createElement("main",null,l.createElement("h1",null,"About Page"),l.createElement("p",null,"This is my about page"),l.createElement(a.Link,{to:"/"},l.createElement("h3",null,"Return Home")),[1,2,3,4].map((e=>l.createElement("p",null,e))));const u=()=>l.createElement("title",null,"About Page")}}]);
-//# sourceMappingURL=component---src-pages-about-js-ddfb55bdb916108f4e9c.js.map
